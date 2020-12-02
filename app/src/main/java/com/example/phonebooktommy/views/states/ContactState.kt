4765c3aeaf6af6d216778704e3ca816b20030912
trickytommy <trickytommy4@gmail.com>
2020-12-02 16:02:40 +0700

    fix layout

--- app/src/main/java/com/example/phonebooktommy/views/states/ContactState.kt
+++ app/src/main/java/com/example/phonebooktommy/views/states/ContactState.kt
@@ -3,11 +3,11 @@ package com.example.phonebooktommy.views.states
 sealed class ContactState {
     data class Loading(val message: String = "Loading...") : ContactState()
     data class Error(val exception: Exception) : ContactState()
-    data class SuccessGetAllTodo(val list: List<TodoModel>) : ContactState()
-    data class SuccessInsertTodo(val todo: TodoModel) : ContactState()
-    data class SuccessUpdateTodo(val todo: TodoModel) : ContactState()
-    data class SuccessDeleteTodo(val todo: TodoModel) : ContactState()
+    data class SuccessGetAllContact(val list: List<TodoModel>) : ContactState()
+    data class SuccessInsertContact(val Contact: TodoModel) : ContactState()
+    data class SuccessUpdateContact(val Contact: TodoModel) : ContactState()
+    data class SuccessDeleteContact(val Contact: TodoModel) : ContactState()
     data class SuccessGetAllFavorite(val list: List<TodoModel>) : ContactState()
-    data class SuccessInsertFavorite(val todo: TodoModel) : ContactState()
-    data class SuccessDeleteFavorite(val todo: TodoModel) : ContactState()
+    data class SuccessInsertFavorite(val Contact: TodoModel) : ContactState()
+    data class SuccessDeleteFavorite(val Contact: TodoModel) : ContactState()
 }
